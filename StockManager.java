3ca762161a9c657fecc4b3d3b2818a56bcbe156c
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-03-03 13:29:04 -0500

    Published Repository with Work from Local Files

--- StockManager.java
+++ StockManager.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+//
 public class StockManager
 {
     // A list of the products.
@@ -26,7 +28,45 @@ public class StockManager
      */
     public void addProduct(Product item)
     {
-        stock.add(item);
+        boolean existingID = false;
+        
+        for (Product product : stock) {
+            if (product.getID() == item.getID()) {
+                existingID = true;
+                System.out.println("Sorry, this ID is already in our Stock");
+                break;
+            }
+            
+        }
+        
+        if (!existingID) {
+            stock.add(item);
+        }
+        
+        
+    }
+    
+    public void printProductDetails(){
+     
+     for (Product product : stock){
+         System.out.println(product.toString());
+         
+        }
+        
+    }
+    
+    public void printBelowStockDetails(int quantity) {
+     
+        for (Product product : stock) {
+         
+         if (product.getQuantity() < quantity) {
+             
+             System.out.println(product.toString());
+             
+            }
+            
+        }
+        
     }
     
     /**
@@ -35,9 +75,6 @@ public class StockManager
      * @param id The ID of the product.
      * @param amount The amount to increase the quantity by.
      */
-    public void delivery(int id, int amount)
-    {
-    }
     
     /**
      * Try to find a product in the stock with the given id.
@@ -46,7 +83,98 @@ public class StockManager
      */
     public Product findProduct(int id)
     {
+        
+        for (Product product : stock){
+         
+            if (product.getID() == id){
+                System.out.println(product.getName() + " returned.");
+                return product;
+            } else 
+            return null;    
+            
+            
+        }
+        
         return null;
+    }
+    
+    public Product findProduct (String _name){
+     
+        for (Product product : stock) {
+         
+            String name = product.getName();
+            
+            if (name.equals(_name)){
+                return product;
+            }
+            
+        }
+        
+        System.out.println("No Product in our stock matches the searched Name");
+        return null;
+        
+    }
+    
+    public void print1To10(){
+     
+        int i = 1;
+        
+        do {
+         
+         System.out.println(i);
+         i++;
+            
+        } while (i <= 10);
+        
+        
+    }
+    
+    public void switchCaseExample(String _menuItemName) {
+        // Super Rough menu search
+        switch (_menuItemName) {
+            
+            case "Burger" :
+            System.out.println("We have burgers!");
+            break;
+            
+            case "Salad" :
+            System.out.println("We Do not have Salads");
+            break;
+            
+            case "Pizza with Pineapple" :
+            System.out.println("I'm sorry, but we must ask you to leave");
+            // Customer.kickOutOfStore();
+            break;
+            
+            default :
+            System.out.println("No Products matched that name");
+            
+        }
+        
+    }
+    
+    public int numberInStock(int id){
+     
+     Product product = findProduct(id);   
+     
+     if (product != null){
+         return product.getQuantity();
+        }
+        
+     else 
+        return 0;
+        
+    }
+    
+    public void delivery(int id, int quantity){
+        
+        Product product = findProduct(id);
+        
+        if (product != null) {         
+         product.increaseQuantity(quantity);   
+         
+        }
+        
     }
     
     /**
@@ -56,15 +184,8 @@ public class StockManager
      * @param id The ID of the product.
      * @return The quantity of the given product in stock.
      */
-    public int numberInStock(int id)
-    {
-        return 0;
-    }
 
     /**
      * Print details of all the products.
      */
-    public void printProductDetails()
-    {
-    }
 }
